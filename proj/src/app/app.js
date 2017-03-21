d24a0250c4fd6f3296db5f8cd36ee204aa1487ea
mosikoo <chenqiuws@gmail.com>
2017-03-21 14:45:17 +0800

    up

--- proj/src/app/app.js
+++ proj/src/app/app.js
@@ -2,7 +2,7 @@ import './app.less';
 <% if (SPA) { %>
 import './routes';
 <% } %>
-
+const { chrome } = window;
 // This is a Chrome only hack
 if (__LOCAL__ && window.chrome && chrome.webstore) {
   // see https://github.com/livereload/livereload-extensions/issues/26
